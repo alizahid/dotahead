0d366ae31289561cc5dc141f30d8a8242971b4cc
Ali Zahid <ali.zahid@live.com>
2023-10-30 11:57:39 +0500

    Refactoring

--- src/components/heroes/talents.tsx
+++ src/components/heroes/talents.tsx
@@ -22,21 +22,21 @@ export function HeroTalents({ hero, style }: Props) {
 
       {chunk(hero.talents, 2).map(([left, right], index) => (
         <View style={tw`h-7 flex-row items-center gap-4`} key={index}>
-          <View style={tw`flex-1`}>
-            <Text size={2} align="right">
-              {left.name}
-            </Text>
-          </View>
+          <Text lines={2} style={tw`flex-1`} size={2} align="right">
+            {left.name}
+          </Text>
 
           <View
             style={tw`h-6 w-6 items-center justify-center rounded-6 bg-gray1`}
           >
-            <Text color="tomato11">{labels[index]}</Text>
+            <Text color="tomato11" leading="tight">
+              {labels[index]}
+            </Text>
           </View>
 
-          <View style={tw`flex-1`}>
-            <Text size={2}>{right.name}</Text>
-          </View>
+          <Text lines={2} style={tw`flex-1`} size={2}>
+            {right.name}
+          </Text>
         </View>
       ))}
     </View>
